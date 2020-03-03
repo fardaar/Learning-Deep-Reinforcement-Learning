b74a70b5e20a87964b226022390d35406aa98e81
Ahura Majdinasab <fardaar54@gmail.com>
2020-03-03 11:02:21 +0330

    creat_q_table function

--- Q-Learning/Open-AI's Gym - Frozen Lake/Frozen Lake.py
+++ Q-Learning/Open-AI's Gym - Frozen Lake/Frozen Lake.py
@@ -1,3 +1,17 @@
 import numpy as np
 import gym
 import random
+
+
+def create_q_table(action_size, state_size):
+    # Arguments:
+    #     action_size: the number of actions our agent can take
+    #     state_size: the number of environment's states
+    # Returns:
+    #     q-table: an empty q-table initialized with zeros
+    # Implements:
+    #     creates the agent's q-table
+    return np.zeros((state_size, action_size))
+
+
+env = gym.make('FrozenLake-v0')  # Gym's Frozen Lake environment
