7ac2927d22b065820e334afff30a0237a43ec6ba
Ahura Majdinasab <fardaar54@gmail.com>
2020-03-03 11:10:56 +0330

    defined hyper-parameters

--- Q-Learning/Open-AI's Gym - Frozen Lake/Frozen Lake.py
+++ Q-Learning/Open-AI's Gym - Frozen Lake/Frozen Lake.py
@@ -16,3 +16,12 @@ def create_q_table(action_size, state_size):
 
 
 env = gym.make('FrozenLake-v0')  # Gym's Frozen Lake environment
+q_table = create_q_table(env.action_space.n, env.observation_space.n)  # creating the q-table
+hyper_parameters = {'total_episodes': 15000,  # total steps of training
+                    'learning_rate': 0.8,  # learning rate
+                    'max_steps': 99,  # maximum number of steps before restarting the environment
+                    'gamma': 0.95}  # discount factor
+exploration_parameters = {'epsilon': 1.0,  # exploration rate
+                          'max_epsilon': 1.0,  # maximum exploration rate
+                          'min_epsilon': 0.01,  # minimum exploration rate
+                          'decay_rate': 0.005}  # the rate at which epsilon is to decay at each iteration
