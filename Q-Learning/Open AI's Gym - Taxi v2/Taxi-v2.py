9b74cc94b6110325ae517340c8ccf99c2817c4b9
Ahura Majdinasab <fardaar54@gmail.com>
2020-03-04 08:14:00 +0330

    create_qtable function

--- Q-Learning/Open AI's Gym - Taxi v2/Taxi-v2.py
+++ Q-Learning/Open AI's Gym - Taxi v2/Taxi-v2.py
@@ -1,3 +1,10 @@
 import numpy as np
 import gym
 import random
+
+
+def create_qtable(action_space_size, observation_space_size):
+    return np.zeros((observation_space_size, action_space_size))
+
+
+env = gym.make('Taxi-v2')
