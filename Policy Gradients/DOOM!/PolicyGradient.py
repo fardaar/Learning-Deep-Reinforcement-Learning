bf615fa6ea1a7d586f222c3a7f0484a54718a177
Ahura <fardaar54>
2020-04-04 15:57:14 +0430

    Added documentation

--- Policy Gradients/DOOM!/PolicyGradient.py
+++ Policy Gradients/DOOM!/PolicyGradient.py
@@ -4,56 +4,54 @@ import NeuralModel
 import environment
 import stack_frames
 from collections import deque
+import play_episode
 
 
 class PGAgent:
-    def __init__(self, state_shape, actions, hyperparams):
-        self.input_shape = state_shape
-        self.possible_actions = actions
+    # the agent training with the simplest policy gradient algorithm
+    def __init__(self, state_shape, actions, hyperparams, batch_size):
+        self.batch_size = batch_size  # number of episodes to train the agent on in each iteration
+        self.input_shape = state_shape  # agent's NN input shape
+        self.possible_actions = actions  # actions which the agent can take in environment
         self.num_actions = len(self.possible_actions[0])
-        self.learning_rate = hyperparams['learning_rate']
-        self.discount_rate = hyperparams['discount_rate']
-        self.model = NeuralModel.NeuralModel(self.input_shape, self.num_actions)
-        self.optimizer = tf.keras.optimizers.Adam(lr=self.learning_rate)
+        self.learning_rate = hyperparams['learning_rate']  # learning rate
+        self.discount_rate = hyperparams['discount_rate']  # discount rate
+        self.model = NeuralModel.NeuralModel(self.input_shape, self.num_actions)  # agent's NN
+        self.optimizer = tf.keras.optimizers.Adam(lr=self.learning_rate)  # adam optimizer
+        self.states = deque(maxlen=batch_size)  # agent's memory for the states it has visited
+        self.actions = deque(maxlen=batch_size)  # agent's memory for the action it has taken in each state
+        self.discounted_rewards = deque(maxlen=batch_size)  # discounted rewards of the actions taken in states
 
-    def predict_action_probs(self, state):
-        return self.model(state)
+    def predict_action_probs(self, state, in_training):
+        # returns action probabilities if it's not training and logits if otherwise
+        return self.model(state, in_training)
 
-    @tf.function
-    def train(self, episode):
-        actions = episode['actions']
-        rewards = episode['rewards']
+    def train(self):
+        # train the agent on what it has stored in its memory
+        for j in range(self.batch_size):
+            states, actions, discounted_rewards = self.states[j], self.actions[j], self.discounted_rewards[j]
 
-        with tf.GradientTape() as tape:
-            loss = -tf.math.log(actions) * rewards
+            with tf.GradientTape() as tape:
+                neg_log_probs = tf.nn.softmax_cross_entropy_with_logits(actions,
+                                                                        self.model(np.asarray(states),
+                                                                                   in_training=True))
+                loss = tf.math.reduce_mean(tf.math.multiply(neg_log_probs, discounted_rewards))
 
-        variables = self.model.trainable_variables
-        gradients = tape.gradient(loss, variables)
-        self.optimizer.apply_gradients(zip(gradients, variables))
-
-    def play(self):
-        episode = {'actions': [], 'rewards': []}
-        game, possible_actions = environment.create_environment()
-        game.new_episode()
-
-        first_state = game.get_state()
-        stacked_states = deque([np.zeros((84, 84), dtype=np.int) for _ in range(4)], maxlen=4)
-        stacked_states = stack_frames.stack_frames(stacked_states, first_state.screen_buffer, True)
-        action_tensor = self.predict_action_probs(np.asarray(stacked_states).reshape(1, 84, 84, 4))
-        first_action = possible_actions[tf.math.argmax(action_tensor, axis=1).numpy()[0]]
-        first_reward = game.make_action(first_action)
-        episode['actions'].append(action_tensor)
-        episode['rewards'].append(first_reward)
-        self.train(episode)
-        while not game.is_episode_finished():
-            state = game.get_state()
-            stacked_states = stack_frames.stack_frames(stacked_states, state.screen_buffer, False)
-            action_tensor = self.predict_action_probs(np.asarray(stacked_states).reshape(1, 84, 84, 4))
-            action = tf.math.argmax(action_tensor, axis=1).numpy()[0]
-            reward = game.make_action(action)
-            episode['actions'].append(action_tensor)
-            episode['rewards'].append(reward)
+            variables = self.model.trainable_variables
+            gradients = tape.gradient(loss, variables)
+            self.optimizer.apply_gradients(zip(gradients, variables))
 
 
-agent = PGAgent([84, 84, 4], [[0, 0, 1], [0, 1, 0], [1, 0, 0]], {'learning_rate': 0.01, 'discount_rate': 0.01})
-agent.play()
+agent = PGAgent([84, 84, 4], [[1, 0, 0], [0, 1, 0], [0, 0, 1]], {'learning_rate': 0.00001, 'discount_rate': 0.95},
+                batch_size=5)
+while True:
+    for i in range(agent.batch_size):
+        temp_states, temp_actions, temp_discounted_rewards = play_episode.play_episode(agent)
+        agent.states.append(temp_states)
+        agent.actions.append(temp_actions)
+        agent.discounted_rewards.append(temp_discounted_rewards)
+        print(i)
+    agent.train()
+    agent.states.clear()
+    agent.actions.clear()
+    agent.discounted_rewards.clear()
